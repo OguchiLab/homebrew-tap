7a2eae3d50bfba3a10db265877f48410283a5b2f
Masaki Ito <mito@iis.u-tokyo.ac.jp>
2021-11-03 10:52:43 +0900

    Create new Formula rabbitmq-stableb

---
+++ Formula/rabbitmq-stable.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class RabbitmqStable < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.23/rabbitmq-server-generic-unix-3.8.23.tar.xz"
+  sha256 "943a8e1399cf7d9f30306be5d51acf60964c4308b3ed118e1f82b7fca9970ba7"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test rabbitmq-stable`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
